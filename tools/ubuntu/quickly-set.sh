8cf9db460f1c56167335d56ba3aa85b95f6198fb
liufei <flying0216@gmail.com>
2012-07-09 21:59:45 +0800

    update quickly-set

--- tools/ubuntu/quickly-set.sh
+++ tools/ubuntu/quickly-set.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
-#在右键里面 添加一个“打开终端”的菜单
+#在右键里面 添加一个“打开终端”的菜单,需重启图形界面
 sudo apt-get install nautilus-open-terminal
-#右键中添加"以管理员身份打开"
+#右键中添加"以管理员身份打开"，需重启图形界面
 sudo apt-get install nautilus-gksu
+
+#rar，7z支持
+sudo apt-get install rar
+sudo apt-get install p7zip
+
 #重启图形界面
 sudo service gdm restart
+
 
--- tools/ubuntu/setfont.sh
+++ tools/ubuntu/setfont.sh
@@ -6,7 +6,7 @@ if [ ! -d $CreatDir ] ; then
     sudo mkdir $CreatDir
 fi
 
-sudo cp /media/027E04147E03FF63/Windows/Fonts/msyh* $CreatDir
+sudo cp /media/$1/Windows/Fonts/msyh* $CreatDir
 sudo chmod 755 $CreatDir/*
 cd $CreatDir
 sudo mkfontscale && sudo mkfontdir && sudo fc-cache -fv > /dev/null

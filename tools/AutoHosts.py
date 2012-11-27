5239e0eef99d40c5a6f23732e31f31959660ea66
liufei <flying0216@gmail.com>
2012-11-27 16:59:13 +0800

    2012年 11月 27日 星期二 16:59:13 CST: autoetc.

--- tools/AutoHosts.py
+++ tools/AutoHosts.py
@@ -3,44 +3,60 @@ import platform
 import socket
 import fileinput
 import sys
+import os
 
 hostsURL = "https://smarthosts.googlecode.com/svn/trunk/hosts"
 hostName = socket.gethostname()
 sysInfo = platform.system()
+strDes = ""
+strUser = ""
 
 def UsePlatform():
   global sysInfo
+  global strDes
+  global strUser
   if(sysInfo == "Windows"):
     strDes = "C:\Windows\System32\drivers\etc\hosts"
+    strUser = "C:\Windows\System32\drivers\etc\PrivateHosts"
   if(sysInfo == "Linux"):
     strDes = "/etc/hosts"
-  return strDes
-    
+    strUser = "/etc/PrivateHosts"
+  if(os.path.exists(strUser) == False):
+    files = open(strUser, "w")
+    files.write("")
+    files.close()
+
 def LinuxPlatform(strDir):
-  replaceStr="localhost"
+  replaceStr = "localhost"
   for line in fileinput.input(strDir, inplace=1):
     line = line.replace(replaceStr, hostName)   
     sys.stdout.write(line)
 
-def WriteHosts(strDes):
+def WriteHosts():
+  global strDes
   # get code of given URL as html text string
   # Python3 uses urllib.request.urlopen()
   # instead of Python2's urllib.urlopen() or urllib2.urlopen()
   getHosts = urllib.request.urlopen(hostsURL)
   mybytes = getHosts.read()
-
   # note that Python3 does not read the html code as string
   # but as html code bytearray, convert to string with
   strHosts = mybytes.decode("utf8")
+  # Add User Hosts
+  FileUser = open(strUser, "r")
+  contents = FileUser.read()
+  FileUser.close()
+  strHosts = strHosts + "\n" + contents
 
   FileHosts = open(strDes, "w")
   FileHosts.write(strHosts)
   FileHosts.close()
   getHosts.close()
 
+
 def main():
-  strDes = UsePlatform()
-  WriteHosts(strDes)
+  UsePlatform()
+  WriteHosts()
   # support linux platform
   if(sysInfo == "Linux"):
     LinuxPlatform(strDes)

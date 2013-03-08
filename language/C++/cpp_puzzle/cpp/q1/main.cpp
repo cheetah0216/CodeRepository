1c65e38cec3baa24eba0bc8d34ee3c4ed787cf8c
root <root@liufei-ThinkPad.(none)>
2013-03-08 09:00:26 +0800

    cpp

--- language/C++/cpp_puzzle/cpp/q1/main.cpp
+++ language/C++/cpp_puzzle/cpp/q1/main.cpp
@@ -1,7 +1,24 @@
-#include <pthread.h>
 #include "singleton.h"
 
 Singleton* Singleton::m_pInstance = NULL;
+Locker Singleton::locker;
+
+void * thr_fn1(void *arg)
+{
+    cout << "this is thread 1!" << endl;
+    for( int i =0 ; i < 20; i++)
+    	Singleton::GetInstance();  
+    return ((void*)1);
+}
+
+void * thr_fn2(void *arg)
+{
+    cout << "this is thread 2!" << endl;
+    for( int i =0 ; i < 20; i++)
+    	Singleton::GetInstance(); 
+    return ((void*)1);
+}
+
 int main(int argc, char ** argv)
 {
 //	Singleton::GetInstance();  
@@ -12,9 +29,9 @@ int main(int argc, char ** argv)
 	
 	Singleton::Release(); 
 
-//带添加多线程
-	int err;
 	pthread_t tid1,tid2;
-//	pthread_create();
+	pthread_create(&tid1, NULL, thr_fn1,NULL);
+	pthread_create(&tid2, NULL, thr_fn2,NULL);
+	sleep(1);
 	return 0;
 }
--- language/C++/cpp_puzzle/cpp/q1/singleton.h
+++ language/C++/cpp_puzzle/cpp/q1/singleton.h
@@ -1,14 +1,28 @@
 // Define your singleton class in this file
 #include <iostream>
+#include <pthread.h>
 using namespace std;
 
-class Singleton {
+class Locker  
+{  
+    public:  
+    	inline Locker(){        pthread_mutex_init(&mutex,NULL);}  
+    	inline ~Locker(){       pthread_mutex_destroy(&mutex);}  
+    	inline void Lock(){     pthread_mutex_lock(&mutex);}  
+    	inline void Unlock(){   pthread_mutex_unlock(&mutex);}  
+    private:  
+    	pthread_mutex_t mutex;  
+}; 
+
+class Singleton 
+{
     public:
         static Singleton* GetInstance();
 	static void Release();
     private:
         Singleton();
 	static Singleton* m_pInstance;
+	static Locker locker; 
 };
 
 Singleton::Singleton()  
@@ -17,12 +31,17 @@ Singleton::Singleton()
 }  
   
 Singleton* Singleton::GetInstance()  
-{  
-    if ( m_pInstance == NULL )  
-    {  
-        m_pInstance = new Singleton();
-	cout << "This is Singleton's instance." << endl;    
-    }  
+{   
+    if ( m_pInstance == NULL ) 
+    {
+	locker.Lock();
+        if ( m_pInstance == NULL )  
+        {  
+            m_pInstance = new Singleton();
+	    cout << "This is Singleton's instance." << endl;    
+        }  
+	locker.Unlock();
+    }
     return m_pInstance;  
 }  
 

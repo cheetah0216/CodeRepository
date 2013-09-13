444f42f85ad8aa56cffe5241a9a321898bc665e6
fliu <flying0216@gmail.com>
2013-09-13 14:51:32 +0800

    2013年 09月 13日 星期五 14:51:32 CST: autoetc.

--- FrontEnd/node.js/search.js
+++ FrontEnd/node.js/search.js
@@ -2,49 +2,17 @@ var http = require('http'),
     querystring = require('querystring');
 
 function search() {
-
-    var options = {
-      hostname: '10.8.33.86',
-     // path: '/phones/phone.cfm?',
-      path: '/phones/phone.cfm?ShowName=yes',
-      method: 'PUT'
-      //headers: {
-        //Connection: 'keep-alive',
-        //Accept: 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
-        //Origin: 'http://10.8.33.86',
-        //Referer: 'http://10.8.33.86/phones/phone.cfm?CFID=3304492&CFTOKEN=94824512'
-     // }
-    };
-    
-
-    console.log('http.request');
-    var req = http.request(options, function(res) {
-        var resData = "";
-        console.log("statusCode: ", res.statusCode);
-        res.on('data', function (chunk){
-            console.log('res.on');
-            resData += chunk;
-            console.log(querystring.parse(resData));
-        });
-
-        req.on('end', function(e) {
-            console.log('req.end');
-            console.log(querystring.parse(resData));
-        });
-                
-        });
-
-   req.on('error', function (e) {
-        console.log('problem with request: ' + e.message)
-        });
-
-    var contents =  querystring.stringify({
-       name: 'fei',
-        submit: 'go'
+  var url = "http://10.8.33.86/phones/phone.cfm?ShowName=yes&name=fei&submit=go";
+  var req = http.get(url, function(res) {
+    console.log('STATUS: ' + res.statusCode);
+    console.log('HEADERS: ' + JSON.stringify(res.headers));
+    res.setEncoding('utf8');
+    res.on('data', function (chunk) {
+      console.log('BODY: ' + chunk);
     });
-    req.write(contents);
-    req.end();
-
+  });
+  req.on('error',function(e) {
+    console.log("Got error: " + e.message);
+  });
 }
-
 search();

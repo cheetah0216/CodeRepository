86742e77137b1d5c20b8bc7a914639c57ae31d2f
fliu <flying0216@gmail.com>
2014-11-10 20:33:59 +0800

    lbs

--- lflib/c++/lbs/shm.cpp
+++ lflib/c++/lbs/shm.cpp
@@ -25,7 +25,9 @@ typedef bgi::quadratic<32> qu;
 typedef bgi::linear<32, 8> par;
 typedef bgi::rtree< value, par, idx, eq, alloc > Rtree;
 
-Rtree * rtree;
+bi::managed_mapped_file *_file;
+alloc *_ac;
+Rtree *rtree; 
 
 uint64_t get_microstamp()
 {
@@ -63,35 +65,45 @@ int get_randnum(const unsigned int randmax)
 
 void create_rtree()
 {
+    std::cout << "create:" << std::endl;
     int posx = 0;
     int posy = 0;
     uint64_t starttime;
     starttime = get_microstamp();
 
-    for(int i = 0; i < 1000; i++){
-        posx = get_randnum(9000001);
-        posy = get_randnum(18000001); 
+    for(int i = 0; i < 1; i++){
+        //posx = get_randnum(9000001);
+        //posy = get_randnum(18000001); 
+        posx = 1;
+        posy = 2;
         //std::cout << i << ": " << posx << "," << posy << std::endl;
         //std::cout << i << ": " << posx - 1 << "," << posy - 1 << std::endl;
 
         point b(posx,posy);
 
-        std::string str1 = "hello";
-        std::string str2 = "luxy";
-        rtree->insert(std::make_pair(point(posx, posy), str1));
-        rtree->insert(std::make_pair(point(posx, posy), str2));
+        try{
+            std::cout << rtree->size() << std::endl;
+            std::string str1 = "hello";
+            std::string str2 = "luxy";
+            rtree->insert(std::make_pair(point(posx, posy), str1));
+            rtree->insert(std::make_pair(point(posx, posy), str2));
+        }catch(...){
+            std::cout << "failed!" << std::endl;
+            //_file->grow("data.bin", 1024 * 1024);
+        }
+
     }
 
     uint64_t endtime = get_microstamp();
     std::cout << "create rtree time:" << (endtime - starttime) << "ms" << std::endl;
 }
 
-void test_point()
+void test_point(int x, int y)
 {
     uint64_t starttime;
     starttime = get_microstamp();
  
-    point p(7092605, 16092663);
+    point p(x, y);
     std::vector<value> result_s;
     rtree->query(bgi::within(p), std::back_inserter(result_s));
 
@@ -103,6 +115,39 @@ void test_point()
         std::cout << bg::wkt<point>(v.first) << " - " << v.second << std::endl;
 
 }
+
+void test_del(int x, int y)
+{
+    std::string val = "hello";
+    if(rtree->remove(std::make_pair(point(x, y), val))){
+        std::cout << "remove success!" << std::endl;
+    }else{
+        std::cout << "recome failed!" << std::endl;
+    }
+}
+
+void test_remove(int x, int y)
+{
+    point p(x, y);
+    std::vector<value> result_s;
+    rtree->query(bgi::within(p), std::back_inserter(result_s));
+    unsigned num1 = result_s.size();
+    BOOST_FOREACH(value const& v, result_s)
+    {
+        std::cout << bg::wkt<point>(v.first) << " - " << v.second << std::endl;
+        rtree->remove(std::make_pair(v.first, v.second));
+    }
+
+    result_s.clear();
+    rtree->query(bgi::within(p), std::back_inserter(result_s));
+    unsigned num2 = result_s.size();
+
+    if(num1 == 2 && num2==0){
+        std::cout << "delete success" << std::endl;
+    }
+}
+
+
 
 void test_range()
 {
@@ -128,7 +173,7 @@ void test_near()
 {
     uint64_t starttime = get_microstamp();
     std::vector<value> result_n;
-    rtree->query(bgi::nearest(point(0, 0), 10000), std::back_inserter(result_n));
+    rtree->query(bgi::nearest(point(0, 0), 100), std::back_inserter(result_n));
 
     uint64_t endtime = get_microstamp();
     std::cout << "time:" << (endtime - starttime) << "ms" << std::endl;
@@ -142,14 +187,19 @@ void test_near()
 
 int main()
 {
-    bi::managed_mapped_file file(bi::open_or_create, "data.bin", 1024*1024);
-    alloc ac(file.get_segment_manager());
-	rtree = file.find_or_construct<Rtree>("rtree")(par(), idx(), eq(), ac);
-
+    _file = new bi::managed_mapped_file(bi::open_or_create, "data.bin", 100*1024*1024);
+    _ac = new alloc(_file->get_segment_manager());
+    rtree = _file->find_or_construct<Rtree>("rtree")(par(), idx(), eq(), *_ac);
 	create_rtree();    
-    test_range();
+    //test_range();
     test_near();
-    test_point();
+    //test_point(-5867798, -14867835);
+    //test_remove(1, 2);
+    test_del(1, 2);
+    test_near();
+    //test_remove(2,2);
+    test_del(1, 2);
+    test_near();
 
     return 0;
 }
